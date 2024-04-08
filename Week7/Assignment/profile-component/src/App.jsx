19e87f7ec9510bdfc2c685a1183c8af0e2355b21
haqueinsham@gmail.com <haqueinsham@gmail.com>
2024-04-09 01:25:48 +0530

    ok

---
+++ Week7/Assignment/profile-component/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <Profile />
+    </div>
+  )
+}
+function Profile(){{
+  return(
+    <Header />
+  )
+}}
+function Header(){
+  <div className='header'>
+
+  </div>
+}
+function photo(){
+
+}
+function count(){
+
+}
+
+export default App
